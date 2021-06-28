b4079619a4de1700355f1ec42c8e45201f24cc61
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-28 11:34:02 -0400

    the basics of writing custom Java classes that interact with another

---
+++ src/ClassOne.java
@@ -0,0 +1,19 @@
+
+public class ClassOne {
+	int id;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public static void main(String[] args) {
+		ClassOne c1 = new ClassOne();
+		c1.setId(25);
+		
+		System.out.println("id is: " + c1.getId());
+	}
+}
